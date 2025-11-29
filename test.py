d1957a5f89621c20dd5a40d2513e485a9ea58813
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-11-29 10:55:02 +0500

    ok

---
+++ test.py
@@ -0,0 +1,19 @@
+from datetime import date
+from library.create_tables import init_db
+from library.services import (
+    create_author,
+    create_book,
+    get_all_authors,
+    get_all_books,
+    get_author_by_id,
+    get_book_by_id,
+    search_books_by_title,
+    delete_author,
+    delete_book
+)
+
+init_db()
+
+
+ok=create_author(name="leo", bio="ok")
+print(ok)
